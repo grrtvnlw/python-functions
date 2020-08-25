a13079393005ce7f02f4bfd49aab5dc06003add2
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-08-24 23:07:16 -0400

    refactors

--- test.py
+++
@@ -1,9 +0,0 @@
-use_this = [2,32,534,23,24,345,23]
-def smallest(list_of_numbers):
-    smallest_number = []
-    for num in list_of_numbers :
-        smallest_number.append(num)
-    smallest_number = smallest_number.sort()
-    print(smallest_number)
-    return smallest_number
-print(smallest(use_this))
